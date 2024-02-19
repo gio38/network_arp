1d82b489e4f06d1a10370ca1257ce51c17155307
gio <giovanni.correa@sartorius.com>
2024-02-18 21:37:21 -0400

    second commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from utils import get_ip_list
+from utils import get_ip_from_arp_table
 from netmiko import ConnectHandler
 from credentials import *
 from pprint import pprint as pp
@@ -20,13 +21,16 @@ from pprint import pprint as pp
 #
 # # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
-dc01_arp_ip_list = list()
-dc02_arp_ip_list = list()
-
+dc01_arp_ip_list: list = list()
+dc02_arp_ip_list: list = list()
+same_ip: list = list()
+ip_dc01_not_dc02: list = list()
+ip_dc02_not_dc01: list = list()
 # d = ConnectHandler()
 
 try:
     ip_list = get_ip_list()
+    print(ip_list)
 
     for ip in ip_list:
         device = ConnectHandler(device_type="aruba_osswitch", ip=ip, username=username, password=password)
@@ -35,22 +39,60 @@ try:
 
         edit_arp_result = arp_result.splitlines()
 
-        print(edit_arp_result)
-        print("\n\n")
+        # print(edit_arp_result)
+        # print(f"\n --- {ip} ---\n")
 
-        pp(edit_arp_result)
+        # pp(edit_arp_result)
 
-        # for i in edit_arp_result[5:-1]:
-        #     a = i.split()
-        #     # print(a[0])
-        #     arp_ip_list.append(a[0])
+        if len(dc01_arp_ip_list) == 0:
+            print(f"------------------ Connected to DC 01 ip: {ip} ------------------")
+            dc01_arp_ip_list = get_ip_from_arp_table(edit_arp_result)
+            # print(ip)
+            # for i in edit_arp_result[5:-3]:
+            #     a = i.split()
+            #     # print(a[0])
+            #     print("------------- DC 01 ----------------")
+            #     dc01_arp_ip_list.append(a[0])
+        else:
+            print(f"------------------ Connected to DC 02 ip: {ip} -------------------")
+            dc02_arp_ip_list = get_ip_from_arp_table(edit_arp_result)
+            # print(ip)
+            # for i in edit_arp_result[5:-3]:
+            #     a = i.split()
+            #     # print(a[0])
+            #     print("------------- DC 02 -----------------")
+            #     dc02_arp_ip_list.append(a[0])
 
+        print("------------------------ Disconnect -------------------------")
         device.disconnect()
-
 finally:
 
-    print('pass \n ---------------------------- \n')
-    # print(sorted(arp_ip_list))
+    pp(sorted(dc01_arp_ip_list))
+    print('\n ---------------------------- \n')
+    pp(sorted(dc02_arp_ip_list))
+
+    for ip in dc01_arp_ip_list:
+        if ip in dc02_arp_ip_list:
+            if ip not in same_ip:
+                same_ip.append(ip)
+
+    for ip in dc01_arp_ip_list:
+        if ip not in dc02_arp_ip_list:
+            ip_dc01_not_dc02.append(ip)
+
+    for ip in dc02_arp_ip_list:
+        if ip not in dc01_arp_ip_list:
+            ip_dc02_not_dc01.append(ip)
+
+    print("\n --------- SAME IP's ----------")
+    pp(sorted(same_ip))
+
+    print("\n --------- IP's in DC 01 not in  DC 02 --------")
+    pp(sorted(ip_dc01_not_dc02))
+
+    print("\n -------- IP's in DC 02 not in DC 01 ---------")
+    pp(sorted(ip_dc02_not_dc01))
+
     # d.disconnect()
 
 
--- utils.py
+++ utils.py
@@ -4,3 +4,13 @@ def get_ip_list() -> list:
         ip_list = ip_file.read().splitlines()
     return ip_list
 
+
+def get_ip_from_arp_table(edit_arp_result: list) -> list:
+    ip_list: list = []
+    for i in edit_arp_result[5:-3]:
+        a = i.split()
+        # print(a[0])
+        # print("------------- DC 01 ----------------")
+        ip_list.append(a[0])
+
+    return ip_list
